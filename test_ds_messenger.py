5cb1904fb6095d36ecd6bc8f5e05c1967c89be51
Mihir Katyal <mkatyal@uci.edu>
2024-03-20 18:03:34 -0700

    added retries to the test_ds_messenger.py test

--- test_ds_messenger.py
+++ test_ds_messenger.py
@@ -11,3 +11,11 @@ def test_authentication():
     print("Testing authentication...")
     messenger = DirectMessenger(dsuserver=DSUSERVER, username=TEST_USERNAME, password=TEST_PASSWORD)
     assert messenger.authenticate(), "Failed to authenticate"
+
+def test_retrieve_new():
+    print("Testing Retrieving New Messages...")
+    messenger = DirectMessenger(dsuserver=DSUSERVER, username=TEST_USERNAME, password=TEST_PASSWORD)
+    assert messenger.authenticate(), "Pre-Test Authentication failed."
+    new_messages = messenger.retrieve_new()
+    assert isinstance(new_messages, list), "Failed to retrieve new messages as a list."
+    print(f"Retrieved {len(new_messages)} new messages.")
