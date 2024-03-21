ec4672bf1d6395c73124996a439a733b21e11d0c
Mihir Katyal <mkatyal@uci.edu>
2024-03-20 18:03:12 -0700

    added server configuaration to test_ds_messenger.py

--- test_ds_messenger.py
+++ test_ds_messenger.py
@@ -1,3 +1,13 @@
 import time
 from ds_messenger import DirectMessenger
 
+# Configuartion for testing
+DSUSERVER = "168.235.86.101"
+TEST_USERNAME = "f21demo"
+TEST_PASSWORD = "pwd123"
+RECIPIENT_USERNAME = "recipient_username"  # Ensure this is an account you can check
+
+def test_authentication():
+    print("Testing authentication...")
+    messenger = DirectMessenger(dsuserver=DSUSERVER, username=TEST_USERNAME, password=TEST_PASSWORD)
+    assert messenger.authenticate(), "Failed to authenticate"
